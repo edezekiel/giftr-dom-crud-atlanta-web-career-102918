221d0e64bb6a4d85e59bc8af68aeaed5a1d7f8a3
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-04-27 16:35:25 -0500

    add names of gift items to dom

--- src/index.js
+++ src/index.js
@@ -8,5 +8,8 @@ function showGifts() {
   giftNode = document.querySelector('.gift-list')
   for (let i = 0; i < gifts.length; i++) {
     console.log(gifts[i])
+    let gift = document.createElement('li')
+    gift.innerText = gifts[i].name
+    giftNode.appendChild(gift)
   }
 }
