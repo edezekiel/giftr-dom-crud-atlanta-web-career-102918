18e941917014eb4c9398d53a94bf40812abdb076
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-04-27 16:32:59 -0500

    loop over each gift in gifts array

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM has been fully loaded')
   console.table(gifts)
+  showGifts()
 })
+
+function showGifts() {
+  giftNode = document.querySelector('.gift-list')
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(gifts[i])
+  }
+}
