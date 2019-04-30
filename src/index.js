1cdf8d7b0f73f7f6bfa45d7942309f93111a8442
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-04-30 16:19:48 -0500

    delete console.log statement

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
 function showGifts() {
   giftNode = document.querySelector('.gift-list')
   for (let i = 0; i < gifts.length; i++) {
-    console.log(gifts[i])
     let gift = document.createElement('li')
     gift.innerText = gifts[i].name
     giftNode.appendChild(gift)
